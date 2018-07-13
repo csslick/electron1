3a10879f3d35b5784cd55358f885b18c180a3d3b
csslick <digitone@naver.com>
2018-07-13 16:13:53 +0900

    Add files via upload

--- index4.js
+++ index4.js
@@ -15,6 +15,11 @@ function boot(){
 
   // 불러올 문서 파일 경로(file, url)
   win.loadURL(`file://${__dirname}/index.html`);
+
+  // 윈도우 종료시 메모리 초기화
+  win.on('closed', ()=>{
+    win = null;
+  });
 }
 
 // Main and renderer
